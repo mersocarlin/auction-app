076cf40cdb69f838dc8468bdbacce7df605c3892
mersocarlin <mersocarlin@gmail.com>
2016-02-20 12:39:22 -0200

    Add buyers to Home container

--- app/containers/home.js
+++ app/containers/home.js
@@ -1,12 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
+
+import Buyer from '../components/buyer';
 
 
 class Home extends Component {
+  static propTypes = {
+    history: PropTypes.object.isRequired,
+  };
+
+  handleBuyerClick (id) {
+    this.props.history.push(`/auctions/${id}`);
+  }
+
   render () {
+    const buyersList = [
+      { id: 1, name: 'Buyer 1' },
+      { id: 2, name: 'Buyer 2' },
+    ];
+
+    const buyers = buyersList
+      .map((buyer) => {
+        return (
+          <Buyer
+            key={buyer.id}
+            buyer={buyer}
+            onClick={::this.handleBuyerClick}
+          />
+        );
+      });
+
     return (
       <div>
-        Wellcome to auction-app! ;)
+        Choose your Buyer:
+        {buyers}
       </div>
     );
   }
