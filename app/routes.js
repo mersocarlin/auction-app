7c49128e0cbcd8dbebcaa1f107f620200a86bca5
mersocarlin <mersocarlin@gmail.com>
2016-02-20 12:39:57 -0200

    Update routes

--- app/routes.js
+++ app/routes.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Route } from 'react-router';
 
 import App from './containers/app';
+import AuctionDetail from './containers/auction-detail';
+import Auctions from './containers/auctions';
 import Home from './containers/home';
 import NoMatch from './containers/no-match';
 
@@ -9,6 +11,8 @@ import NoMatch from './containers/no-match';
 export default (
   <Route component={App}>
     <Route path="/" component={Home} />
+    <Route path="/auctions/:buyerId" component={Auctions} />
+    <Route path="/auctions/detail/:buyerId/:auctionId" component={AuctionDetail} />
     <Route path="*" component={NoMatch}/>
   </Route>
 );
