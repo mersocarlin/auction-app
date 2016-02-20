c7ee8a09505b0c39b2bc4509b2d846dc1511fa06
mersocarlin <mersocarlin@gmail.com>
2016-02-20 13:21:12 -0200

    Add styles to Item component

--- app/components/item.js
+++ app/components/item.js
@@ -11,17 +11,22 @@ export default class Auction extends Component {
 
     return (
       <div className="item-component">
-        <div> Item </div>
-        <p>
-          Name: {item.name}
-        </p>
-        <p>
-          Description: {item.description}
-        </p>
-        <p>
-          Price: {item.startingPrice}
-        </p>
-        {/* <img src={item.picture} /> */}
+        <div className="panel panel-default">
+          <div className="panel-heading">
+            <h3 className="panel-title">
+              {item.name}
+            </h3>
+          </div>
+          <div className="panel-body">
+            <p>
+              Description: {item.description}
+            </p>
+            <img className="img-responsive" src={item.picture} />
+          </div>
+          <div className="panel-footer">
+            Price: {item.startingPrice}
+          </div>
+        </div>
       </div>
     );
   }
